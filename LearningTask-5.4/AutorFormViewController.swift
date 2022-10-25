e34336d1280b4401353d76255f7a9f616f16d87b
Debora Rodrigues <debora.souza@zup.com.br>
2022-10-25 16:17:00 -0300

    Criar alerts e validacoes na controller

--- LearningTask-5.4/AutorFormViewController.swift
+++ LearningTask-5.4/AutorFormViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class AutorFormViewController: UIViewController {
-
+    
+    typealias MensagemDeValidacao = String
+    
     @IBOutlet weak var fotoTextField: UITextField!
     @IBOutlet weak var nomeTextField: UITextField!
     @IBOutlet weak var bioTextField: UITextField!
@@ -20,7 +22,14 @@ class AutorFormViewController: UIViewController {
     }
 
     @IBAction func botaoSalvarPressionado(_ sender: UIButton) {
-        print("go")
+        switch formularioEhValido() {
+                    
+                case (false, let mensagem):
+                    exibeAlerta(titulo: "Erro", mensagem: mensagem)
+                    
+                default:
+                    cadastraAutor()
+                }
     }
     
     func nomeDeAutorValido(_ nome: String) -> Bool {
@@ -33,5 +42,48 @@ class AutorFormViewController: UIViewController {
         let nomeCompleto = nomeDeAutor.components(separatedBy: separador)
         return (nomeCompleto.first!, nomeCompleto.dropFirst().joined(separator: separador))
     }
-
+    
+    func formularioEhValido() -> (Bool, MensagemDeValidacao?) {
+            if let fotoUrl = fotoTextField.text, fotoUrl.isEmpty {
+                return (false, "Informe a URL da foto do autor")
+            }
+            
+            if let nome = nomeTextField.text, nome.isEmpty {
+                return (false, "Nome não pode estar em branco")
+            }
+            
+            guard nomeDeAutorValido(nomeTextField.text!) else {
+                return (false, "Informe o nome completo do autor.")
+            }
+            
+            if let bio = bioTextField.text, bio.isEmpty {
+                return (false, "A bio do autor não pode estar em branco")
+            }
+            
+            if let tecnologias = tecnologiasTextField.text, tecnologias.isEmpty {
+                return (false, "Informe as tecnologias sobre as quais o autor escreve")
+            }
+            
+            return (true, nil)
+        }
+    
+    func cadastraAutor() {
+            let (nomeAutor, sobrenomeAutor) = formata(nomeDeAutor: nomeTextField.text!)
+            let autor = Autor(foto: fotoTextField.text!,
+                              nome: nomeAutor,
+                              sobrenome: sobrenomeAutor,
+                              bio: bioTextField.text!,
+                              tecologia: tecnologiasTextField.text!)
+            
+            AutorRepository.salva(autor)
+            exibeAlerta(titulo: "Feito", mensagem: "Autor cadastrado com sucesso")
+        }
+    
+    func exibeAlerta(titulo: String?, mensagem: String?) {
+            let alert = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
 }
